b1283ac2d8d0a2f00046a73d3a70f3473f5999fe
ben-pleysier <ben@pleysier.com.au>
2020-11-10 13:24:53 +1100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const staticCacheName = 'static-v1.0.0';
+const dynamicCacheName = 'dynamic-v1.0.0';
+const assets = [
+    '/'
+]
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    });
+};
+
+// install event
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
